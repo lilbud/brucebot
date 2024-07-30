0eeeecac33e4f985e8a2f8d9600cc505c9f42c73
Brian <bvw20002010@gmail.com>
2024-07-30 17:24:06 -0400

    Update db.py

--- brucebot/cogs/bot_stuff/db.py
+++ brucebot/cogs/bot_stuff/db.py
@@ -7,4 +7,4 @@ from psycopg_pool import AsyncConnectionPool
 async def create_pool() -> AsyncConnectionPool:
     """Create a connection pool for the database."""
     load_dotenv()
-    return AsyncConnectionPool(conninfo=os.getenv("HEROKU_DB_URL"), open=False)
+    return AsyncConnectionPool(conninfo=os.getenv("DATABASE_URL"), open=False)

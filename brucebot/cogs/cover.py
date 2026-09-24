1a569fc578835dd478008e1dc4c4fcb321605d47
Brian <bvw20002010@gmail.com>
2026-09-23 20:11:16 -0400

    fix cover command

--- brucebot/cogs/cover.py
+++ brucebot/cogs/cover.py
@@ -4,118 +4,122 @@ from psycopg.rows import dict_row
 
 
 class Cover(commands.Cog):
-    """Collection of commands for grabbing covers from my Github repo."""
+  """Collection of commands for grabbing covers from my Github repo."""
 
-    def __init__(self, bot: commands.Bot) -> None:
-        """Init Cover cog with bot."""
-        self.bot = bot
-        self.description = "Fetch bootleg covers"
+  def __init__(self, bot: commands.Bot) -> None:
+    """Init Cover cog with bot."""
+    self.bot = bot
+    self.description = "Fetch bootleg covers"
 
-    async def covers_embed(
-        self,
-        ctx: commands.Context,
-        files: list,
-        date: str,
-    ) -> None:
-        """Embed for bootleg covers. Uses pages and viewmenu."""
-        menu = await viewmenu.create_view_menu(ctx, title=f"Covers for: {date}")
+  async def covers_embed(
+    self,
+    ctx: commands.Context,
+    files: list,
+    date: str,
+  ) -> None:
+    """Embed for bootleg covers. Uses pages and viewmenu."""
+    menu = await viewmenu.create_view_menu(ctx, title=f"Covers for: {date}")
 
-        for file in files:
-            embed = await bot_embed.create_embed(
-                ctx,
-                title=f"Covers for {date}",
-            )
+    for file in files:
+      embed = await bot_embed.create_embed(
+        ctx,
+        title=f"Covers for {date}",
+      )
 
-            embed.add_field(name="Source:", value=file["source"], inline=False)
+      embed.add_field(name="Source:", value=file["source"], inline=False)
 
-            embed.set_image(
-                url=file["cover_url"],
-            )
+      embed.set_image(
+        url=file["url"],
+      )
 
-            menu.add_page(embed=embed)
+      menu.add_page(embed=embed)
 
-        await menu.start()
+    await menu.start()
 
-    @commands.hybrid_command(
-        name="cover",
-        description="Get list of covers from my repo based on date.",
-        usage="<date>",
-    )
-    async def get_covers(
-        self,
-        ctx: commands.Context,
-        *,
-        date: str,
-    ) -> list:
-        """Get list of covers from my repo based on date."""
-        async with (
-            await db.create_pool() as pool,
-            pool.connection() as conn,
-            conn.cursor(
-                row_factory=dict_row,
-            ) as cur,
-        ):
-            date = await utils.date_parsing(date)
+  @commands.hybrid_command(
+    name="cover",
+    description="Get list of covers from my repo based on date.",
+    usage="<date>",
+  )
+  async def get_covers(
+    self,
+    ctx: commands.Context,
+    *,
+    date: str,
+  ) -> list:
+    """Get list of covers from my repo based on date."""
+    async with (
+      await db.create_pool() as pool,
+      pool.connection() as conn,
+      conn.cursor(
+        row_factory=dict_row,
+      ) as cur,
+    ):
+      date = await utils.date_parsing(date)
 
-            try:
-                date.strftime("%Y-%m-%d")
-            except AttributeError:
-                embed = await bot_embed.not_found_embed(
-                    command=self.__class__.__name__,
-                    message=date,
-                )
-                await ctx.send(embed=embed)
+      try:
+        date.strftime("%Y-%m-%d")
+      except AttributeError:
+        embed = await bot_embed.not_found_embed(
+          command=self.__class__.__name__,
+          message=date,
+        )
+        await ctx.send(embed=embed)
 
-                return
+        return
 
-            res = await cur.execute(
-                """SELECT cover_url, 'lilbud' AS source FROM "covers"
-                    WHERE event_date=%(date)s""",
-                {"date": date.strftime("%Y-%m-%d")},
-            )
+      res = await cur.execute(
+        """SELECT 
+            c.url,
+            'lilbud' AS source
+          FROM "covers" c
+          left join events e on e.id = c.event_id
+          WHERE e.event_date=%(date)s""",
+        {"date": date.strftime("%Y-%m-%d")},
+      )
 
-            files = await res.fetchall()
+      files = await res.fetchall()
 
-            if len(files) == 0:
-                res = await cur.execute(
-                    """SELECT
-                            n.thumbnail_url AS cover_url,
-                            'Nugs' AS source
-                        FROM nugs_releases n
-                        LEFT JOIN events e ON e.id = n.event_id
-                        WHERE e.event_date = %(date)s""",
-                    {"date": date.strftime("%Y-%m-%d")},
-                )
+      # if len(files) == 0:
+      #     res = await cur.execute(
+      #         """SELECT
+      #                 n.thumbnail_url AS cover_url,
+      #                 'Nugs' AS source
+      #             FROM nugs_releases n
+      #             LEFT JOIN events e ON e.id = n.event_id
+      #             WHERE e.event_date = %(date)s""",
+      #         {"date": date.strftime("%Y-%m-%d")},
+      #     )
 
-                files = await res.fetchall()
+      #     files = await res.fetchall()
 
-        # ViewMenu is only for multiple covers,
-        # single embed is just default embed + image.
-        if len(files) == 1:
-            cover = files[0]["cover_url"]
-            embed = await bot_embed.create_embed(
-                ctx,
-                title=f"Cover for {date}",
-            )
+    # ViewMenu is only for multiple covers,
+    # single embed is just default embed + image.
+    if len(files) == 1:
+      cover = files[0]["url"]
+      embed = await bot_embed.create_embed(
+        ctx,
+        title=f"Cover for {date}",
+      )
 
-            embed.add_field(name="Source:", value=files[0]["source"], inline=False)
+      embed.add_field(name="Source:", value=files[0]["source"], inline=False)
 
-            embed.set_image(
-                url=cover,
-            )
+      embed.set_image(
+        url=cover,
+      )
 
-            await ctx.send(embed=embed)
+      await ctx.send(embed=embed)
 
-        elif len(files) > 1:
-            await self.covers_embed(ctx, files, date)
-        else:
-            embed = await bot_embed.not_found_embed(
-                command=self.__class__.__name__,
-                message=date,
-            )
-            await ctx.send(embed=embed)
+    elif len(files) > 1:
+      await self.covers_embed(ctx, files, date)
+    else:
+      embed = await bot_embed.not_found_embed(
+        command=self.__class__.__name__,
+        message=date,
+      )
+      await ctx.send(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
-    """Load extension into bot."""
-    await bot.add_cog(Cover(bot))
+  """Load extension into bot."""
+  await bot.add_cog(Cover(bot))
